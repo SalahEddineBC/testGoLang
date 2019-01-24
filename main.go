08c5de2d5c420a1ca2b70cb58dfdd42400097464
SalahEddineBC <gs_bachircherif@esi.dz>
2019-01-24 21:36:37 +0100

    added graphql resolver

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	grap "./graphql"
+	api "github.com/SalahEddineBC/gRPCservice/api"
 	"github.com/graphql-go/graphql"
 )
 
@@ -17,7 +21,13 @@ var queryType = graphql.NewObject(
 				Type: graphql.String,
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 					// this is the function excuted when we pass this query
-					return "Hello from GraphQL", nil
+					ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+					defer cancel()
+					response, err := serv.Client.Client.GetReleaseNotes(ctx, &api.Empty{})
+					if err != nil {
+						return nil, err
+					}
+					return response, nil
 				},
 			},
 		},
@@ -30,6 +40,7 @@ var schema, _ = graphql.NewSchema(
 		Query: queryType,
 	},
 )
+var serv grap.Server
 
 // function to handle graphql queries
 func executeQuery(query string, schema graphql.Schema) *graphql.Result {
@@ -48,6 +59,12 @@ type queryStruct struct {
 }
 
 func main() {
+	s, err := grap.NewGraphQLServer("localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	serv = *s
+	_ = serv
 	//we create an endpoint here to listen at /graphql
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		//if we send a post request
@@ -62,11 +79,11 @@ func main() {
 			json.NewEncoder(w).Encode(result)
 		} else {
 			//if we send a GET request (or anyother type) we Handle the query from route parameters
+
 			result := executeQuery(r.URL.Query().Get("query"), schema)
 			// we send back the result
 			json.NewEncoder(w).Encode(result)
 		}
-
 	},
 	)
 	// we start the server to listen on port 3000
